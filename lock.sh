542c11d1741d12465e35fded2ac6a25f8abc8242
Vegard Stenhjem Hagen <vegard.hagen@ntnu.no>
2019-09-05 15:46:40 +0200

    Updated lockscreen script

--- lock.sh
+++ lock.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
-#import -window root -crop 3440x1440 -resize 3440x1440 /tmp/screenshot.png
-import -window root /tmp/screenshot.png
+
+# Inspired from https://github.com/x-zvf/dotfiles/blob/master/scripts/scrlock.sh
+
+pngfile="/tmp/screenlock.png"
+bmpfile="/tmp/screenlock.bmp"
+glitched="/tmp/screenglitch.bmp"
+
+# Take screencapture
+scrot -z $pngfile
+
+# Convert to BMP and pixelate
+convert -spread 12 -rotate -90 $pngfile $bmpfile
+
+for a in {1,2,4,5,10}
+do
+    # Glitch image with sox FROM https://maryknize.com/blog/glitch_art_with_sox_imagemagick_and_vim/
+    sox -t ul -c 1 -r 48k $bmpfile -t ul $glitched trim 0 200s : echo 1 1 $((a*2)) 0.1
+    # Roate by 90 deg
+    convert -scale $((100/(a)))% -scale $((100*(a)))% -rotate 90 $glitched $bmpfile
+done
+
+# Add lock icon, pixelate and convert back to png
+convert -gravity center -font "Hack-Bold-Nerd-Font-Complete-Mono" \
+        -pointsize 200 -draw "text 0,0 ''" -channel RGBA -fill '#bf616a' \
+        $bmpfile $pngfile
+
+
 DATE=$(date +'%Y-%m-%d_%H-%M-%S')
-convert /tmp/screenshot.png -blur 5 -spread 12 -pointsize 70 -fill white -annotate +100+100 "$(date)" /tmp/screenshotblur.png
-rm /tmp/screenshot.png
-i3lock -d -f -c 000000 -i /tmp/screenshotblur.png
+#convert $pngfile -blur 5 -spread 12 -pointsize 70 -fill white -annotate +100+100 "$(date)" $pngfile
+convert $pngfile -pointsize 70 -font "Hack-Bold-Nerd-Font-Complete-Mono" \
+        -channel RGBA -fill '#bf616a' -annotate +100+100 "$(date)" $pngfile
+
+i3lock -e -d -f -c 000000 -i $pngfile
+
+rm $pngfile $bmpfile $glitched
