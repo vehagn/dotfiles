23f4a97e79b51cae675cf3560ef033210914bcc6
Vegard Stenhjem Hagen <vegard.hagen@ntnu.no>
2019-12-18 07:42:15 +0100

    Added lazy startup script.

---
+++ startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+setxkbmap -option compose:ralt
+pacmd set-default-sink 1
+compton -cCGfF -b -i 0.75 --vsync opengl
+#feh  --bg-fill '/home/vegahag/Pictures/Wallpapers/EOz85Eq.jpg'
+feh  --bg-fill '/home/vegahag/Pictures/Wallpapers/TNTM5J5.jpg'
